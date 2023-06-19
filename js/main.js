736fb4633eb905ca4dafc78151cc69f78e6787b3
Patricia Atanasoff <system.informatica365@gmail.com>
2023-06-19 17:28:55 -0300

    header, nav, menu responsive, banner home

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// SHOW/HIDDEN MENU
+const enlaces = document.getElementsByClassName("links-nav")[0]
+const hamburguesa = document.getElementsByClassName("hamb")[0]
+const menuHamburguesa = document.getElementById("hamb")
+
+let open = false
+
+const toggleMenu = () => {
+    enlaces.classList.toggle("links-nav2")
+    enlaces.style.transition = "transform 0.5s ease-in-out"
+}
+
+hamburguesa.addEventListener("click", function () {
+    toggleMenu()
+
+    if (document.querySelector("links-nav.links-nav2")) {
+        open = true
+    } else {
+        open = false
+    }
+})
+
+window.addEventListener("click", function (e) {
+    if (open) {
+        if (e.target !== menuHamburguesa) {
+            toggleMenu()
+            open = false
+        }
+    }
+})
+
+window.addEventListener("resize", function () {
+    if (screen.width > 800) {
+        if (open) {
+            toggleMenu()
+            enlaces.style.transition = "none"
+            open = false
+        }
+    }
+})
+
